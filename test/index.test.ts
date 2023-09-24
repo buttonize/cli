2825a69a03f2b5a6f9e7117dd15ccd998c93fa70
Filip Pýrek <PyrekFilip@gmail.com>
2023-09-24 16:55:44 +0200

    ci: implement automated releases

---
+++ test/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest'
+
+describe('CLI Test', () => {
+	it('should', () => {
+		expect(1).toEqual(1)
+	})
+})
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,28 +2,8 @@
 
 import { defineConfig } from 'vitest/config'
 
-export default defineConfig(() => {
-	return {
-		test: {
-			testTimeout: 30000,
-			exclude: [
-				'demo/cdk/**/*',
-				'node_modules',
-				'dist',
-				'.idea',
-				'.git',
-				'.cache'
-			]
-			// coverage: {
-			// 	cleanOnRerun: true,
-			// 	enabled: true,
-			// 	clean: true,
-			// 	all: true,
-			// 	src: ['src'],
-			// 	exclude: ['**/*.test.ts', 'demo/cdk/**/*'],
-			// 	extension: ['.ts'],
-			// 	100: true
-			// }
-		}
+export default defineConfig({
+	test: {
+		include: ['test/**/*.test.ts']
 	}
 })
